2a86b81c72e9e4c7f506e4de42361e08176d20f1
Tranngocanh1911 <83797409+Tranngocanh1911@users.noreply.github.com>
2021-06-10 09:36:40 +0700

    Add files via upload

---
+++ Only_object/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+class Application
+{
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Application();
+            echo 'alo';
+        }
+        return self::$instance;
+    }
+}
